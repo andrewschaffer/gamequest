fa8e0ed37d323592377a2bfa1bb72d272a901478
andrewschaffer <62401878+andrewschaffer@users.noreply.github.com>
2020-05-19 19:31:53 -0700

    gamequest

---
+++ settings.py
@@ -0,0 +1,30 @@
+# © 2019 KidsCanCode LLC / All rights reserved.
+
+# game options/settings
+TITLE = "Jumpy!"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# Environment options
+GRAVITY = 9.8
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.1
+PLAYER_JUMPPOWER = 10
+
+# Monster properties
+MONSTER_ACC = 0.5
+MONSTER_FRICTION = -0.1
+MONSTER_JUMPPOWER = 10
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+LIGHTGREEN = (150, 255, 150)
+BLUE = (0, 0, 255)
+LIGHTBLUE = (200, 220, 255)
+YELLOW = (255, 255, 0)
